5094f244dbcf76f4475d7daba3fcf498e2db1e04
xiaomi <xiaomi@xiaomi-Lenovo.(none)>
2013-08-30 17:39:59 +0800

    some useful packages that i using

---
+++ my_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### 平时装的一些有用的软件
+
+### iotop用来观察哪些程序在占用磁盘IO
+sudo apt-get install iotop
+
+### htop是比top更有用的工具
+sudo apt-get install htop
+
+### 两个linux下的做图软件
+### gnuplot专门用来画坐标图
+sudo apt-get install gnuplot
+
+### graphviz所想即所得画图工具，画指向关系图非常方便
+sudo apt-get install graphviz
+
+### hping分析封包的CLI工具
+sudo apt-get install hping
